a130c82e2aa800186407ac65f45362593ff182f6
Khan Sumama <Sumama.Khan@strathmore.com>
2025-09-26 12:20:38 +0300

    signup update

--- signin.php
+++ signin.php
@@ -1,7 +1,11 @@
 <?php
 require_once 'ClassAutoLoad.php';
-$ObjLayout->header($conf);
-$ObjLayout->navbar($conf);
-$ObjLayout->banner($conf);
-$ObjLayout->form_content($conf, $ObjForm);
-$ObjLayout->footer($conf);
+$ObjLayouts->header($conf);
+$ObjLayouts->navbar($conf);
+$ObjLayouts->banner($conf);
+
+// Your login form content
+$ObjForms->login();
+
+$ObjLayouts->content($conf);
+$ObjLayouts->footer($conf);
