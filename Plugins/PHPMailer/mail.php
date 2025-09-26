fb8f740b7c34421a3ddd242be4712f4b8128cffc
Khan Sumama <Sumama.Khan@strathmore.com>
2025-09-26 12:23:16 +0300

    error handling

--- Plugins/PHPMailer/mail.php
+++ Plugins/PHPMailer/mail.php
@@ -6,7 +6,7 @@ use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 //Load Composer's autoloader (created by composer, not included with PHPMailer)
-require 'vendor/autoload.php';
+require __DIR__ .  '/../../vendor/autoload.php';
 
 //Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
